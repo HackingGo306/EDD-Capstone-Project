2f34b588817e891d8e74d3de5647ec942144a92d
HackingGo306 <22camerjian@gmail.com>
2026-05-14 18:16:40 -0700

    remove redis TLS

--- model/redis.js
+++ model/redis.js
@@ -2,24 +2,27 @@ const Redis = require("ioredis");
 
 let redisClient;
 
-if (process.env.REDIS_URL) {
-  console.log("Connecting to Redis using REDIS_URL");
-  redisClient = new Redis(process.env.REDIS_URL, {
-    maxRetriesPerRequest: null,
-    enableReadyCheck: false,
-    lazyConnect: true,
-    keepAlive: 10000,
-    tls: {},  // explicitly enable TLS
-    reconnectOnError: () => true,
-    retryStrategy: (times) => Math.min(times * 50, 2000),
-  });
-  module.exports = redisClient;
-} else {
-  redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PW
-  });
+if (!global.redisClient) {
+  if (process.env.REDIS_URL) {
+    console.log("Connecting to Redis using REDIS_URL");
+    console.log("REDIS_URL:", process.env.REDIS_URL);
+    redisClient = new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+      enableReadyCheck: false,
+      lazyConnect: true,
+      keepAlive: 10000,
+      reconnectOnError: () => true,
+      retryStrategy: (times) => Math.min(times * 50, 2000),
+    });
+    module.exports = redisClient;
+  } else {
+    redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PW
+    });
+  }
+  global.redisClient = redisClient;
 }
 
 redisClient.on("connect", () => {
